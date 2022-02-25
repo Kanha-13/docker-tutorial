e7bd756d1da505d07807f7e8875db6bf9a84ac35
Kanha <kanha.agr11@gmail.com>
2022-02-25 14:34:11 +0530

    learnt docker-compose

--- node-express/server.js
+++ node-express/server.js
@@ -2,6 +2,6 @@ const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 8001;
 
-app.get('/', (req, res) => res.send('<h2>Hello There , I am nodemon</h2>'))
+app.get('/', (req, res) => res.send('<h2>Hello, I am nodemon</h2>'))
 
 app.listen(PORT, () => console.log(`Server listening at ${PORT}`));
